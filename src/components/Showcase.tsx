edf4208857c0ed0544c4b913e863b8b34f4bf9d6
shibu <sibashis248@gmail.com>
2018-02-10 17:50:38 +0530

    Remove showcase folder under component folder

--- src/components/Showcase.tsx
+++ src/components/Showcase.tsx
@@ -1,10 +1,10 @@
-import * as React from "react";
-import * as Blueprint from "@blueprintjs/core";
-import { Tab2, Tabs2 } from "@blueprintjs/core";
-import { BaseComponent } from "./basecomponent/BaseComponent";
+import * as React from 'react';
+import * as Blueprint from '@blueprintjs/core';
+import { Tab2, Tabs2 } from '@blueprintjs/core';
+import { BaseComponent } from './basecomponent/BaseComponent';
 
-import { InputNumberShowcase } from "./showcase/InputNumberShowcase";
-import { InputTextShowcase } from "../showcase/InputTextShowcase";
+import { InputNumberShowcase } from '../showcase/InputNumberShowcase';
+import { InputTextShowcase } from '../showcase/InputTextShowcase';
 import {InputTextareaShowcase} from '../showcase/InputTextareaShowcase'
 
 
@@ -14,14 +14,10 @@ export interface ShowcaseProps { compiler: string; framework: string; }
 // State is never set so we use the '{}' type.
 export class Showcase extends React.Component<ShowcaseProps, {}> {
     render() {
-        return (<Tabs2 id="Tabs2Example" vertical={true} >
-            <Tab2 id="rx" title="React" panel={this.renderBaseComponent('React jsReact jsReact jsReact jsReact jsReact jsReact jsReact jsReact jsReact jsReact jsReact jsReact jsReact jsReact jsReact jsReact jsReact jsReact jsReact jsReact jsReact jsReact jsReact jsReact jsReact jsReact jsReact jsReact jsReact jsReact jsReact jsReact jsReact jsReact jsReact jsReact jsReact jsReact jsReact jsReact jsReact jsReact jsReact jsReact jsReact jsReact jsReact jsReact jsReact jsReact jsReact jsReact jsReact jsReact jsReact jsReact jsReact jsReact jsReact jsReact jsReact jsReact jsReact jsReact jsReact jsReact jsReact jsReact jsReact jsReact jsReact jsReact jsReact jsReact jsReact jsReact jsReact js')} />
-            <Tab2 id="ng" title="Angular" panel={this.renderBaseComponent('Angular js')} />
-            <Tab2 id="mb" title="Ember" panel={this.renderBaseComponent('Ember js')} />
-            <Tab2 id="bb" title="Backbone" panel={this.renderBaseComponent('Backbone js')} />
-            <Tab2 id="in" title="Inputnumber" panel={this.renderBaseComponent(< InputNumberShowcase/>)} />
-            <Tab2 id="it" title="InputText" panel={this.renderBaseComponent(<InputTextShowcase />)} />
-            <Tab2 id="textarea" title="TextArea" panel={this.renderBaseComponent( <InputTextareaShowcase />)} />
+        return (<Tabs2 id='Tabs2Example' vertical={true} >
+            <Tab2 id='in' title='Inputnumber' panel={this.renderBaseComponent(< InputNumberShowcase/>)} />
+            <Tab2 id='it' title='InputText' panel={this.renderBaseComponent(<InputTextShowcase />)} />
+            <Tab2 id='textarea' title='TextArea' panel={this.renderBaseComponent( <InputTextareaShowcase />)} />
 
         </Tabs2>)
     }
